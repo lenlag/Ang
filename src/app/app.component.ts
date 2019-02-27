48d6dc933fe1f4a6bfd75ef939e493a12be1d6f9
lenlag <lenlag@yandex.ru>
2019-02-27 09:06:41 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,43 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- // title = 'my awesome app';
-
-  isAuth = false;
-
-  statusOn = 'allumé';
-  statusOff = 'éteint';
-
-  appareils = [
-    {
-      name: 'Machine à laver',
-      status: this.statusOff
-    },
-    {
-      name: 'Télévision',
-        status: this.statusOn
-    },
-    {
-      name: 'Ordinateur',
-      status: this.statusOn
-    }
-  ];
-
-  /*
-  appareilOne = 'Machine à laver';
-  appareilTwo = 'Télévision';
-  appareilThree = 'Ordinateur';
-  */
-
-
-  constructor() { // executé au moment de la création de ce component
-    setTimeout(
-      () => {
-      this.isAuth = true;
-    }, 4000
-  );
-  }
-  onAllumer() {
-    console.log('On allume tout !!!');
-  }
+  title = 'Hello! Here is my first angular project!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyFirstComponent } from './my-first/my-first.component';
-import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFirstComponent,
-    AppareilComponent
+    MyFirstComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
